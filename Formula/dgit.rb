381cbef18bbda9162df19d6ae106e55fd410c8e5
Wes Morgan <wesmorgan@icloud.com>
2020-03-13 12:46:16 -0600

    Fix bad perl edits from GH action

--- Formula/dgit.rb
+++ Formula/dgit.rb
@@ -40,15 +40,6 @@ end
 
 __END__
 url: "https://api.github.com/repos/quorumcontrol/dgit/tarball/v0.0.2-alpha"
-sha256: "3143f1824664c03efbfa11f7c6ea8a9de4d98919a019626feb76c3804be362b7"
-bottles:
-  root_url: "https://api.github.com/repos/quorumcontrol/dgit/releases/24511535/assets"
-url: "https://api.github.com/repos/quorumcontrol/dgit/tarball/v0.0.2-alpha"
-sha256: "18db56e1f13a9bd65128b7ed4ab4e1f567e377d7eb27415318fc5e9726283d1b"
-bottles:
-  root_url: "https://api.github.com/repos/quorumcontrol/dgit/releases/24512980/assets"
-  sha256:
-url: "https://api.github.com/repos/quorumcontrol/dgit/tarball/v0.0.2-alpha"
 sha256: "3f9c0e6e2df05d61b2d97aef0a5e04e4538c39c35787354c7afa6ab9a3d379dc"
 bottles:
   root_url: "https://api.github.com/repos/quorumcontrol/dgit/releases/24513180/assets"
